d7e8778b5faf276dc54772d815e0c1b8f3937107
claudiodamico <cdamico18@gmail.com>
2022-03-18 20:44:16 -0300

    First commit

---
+++ Portafolio/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portafolio.Models;
+using Portafolio.Servicios;
+using System.Diagnostics;
+
+namespace Portafolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IRepositorioProyectos _repositorioProyectos;
+        private readonly IServiciosEmail _serviciosEmail;
+
+        public HomeController(ILogger<HomeController> logger, IRepositorioProyectos repositorioProyectos, IServiciosEmail serviciosEmail)
+        {
+            this._logger = logger;
+            this._repositorioProyectos = repositorioProyectos;
+            this._serviciosEmail = serviciosEmail;
+
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Nombre = "Claudio D'Amico";
+
+            var proyectos = _repositorioProyectos.ObtenerProyectos().Take(3).ToList();
+
+            var modelo = new HomeIndexViewModel() { 
+                Proyectos = proyectos
+            };
+            return View(modelo);
+        }
+        
+        public IActionResult Proyectos()
+        {
+            var proyectos = _repositorioProyectos.ObtenerProyectos();
+            return View(proyectos);
+        }
+
+        [HttpGet]
+        public IActionResult Contacto()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Contacto(ContactoViewModel contactoViewModel)
+        {
+            await _serviciosEmail.Enviar(contactoViewModel);
+            return RedirectToAction("Gracias");
+        }
+
+        public IActionResult Gracias()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Portafolio/Servicios/RepositorioProyectos.cs
@@ -0,0 +1,46 @@
+﻿using Portafolio.Models;
+
+namespace Portafolio.Servicios
+{
+    public interface IRepositorioProyectos
+    {
+        List<Proyecto> ObtenerProyectos();
+    }
+
+    public class RepositorioProyectos : IRepositorioProyectos
+    {
+        public List<Proyecto> ObtenerProyectos()
+        {
+            return new List<Proyecto>() {
+                new Proyecto
+            {
+                Titulo = "Divinity Fashion Look",
+                Descripcion = "E-Commerce realizada en Wordpress",
+                Link = "https://pelucasdivinityfashionlook.com/",
+                ImagenURL = "/imagenes/pelucas.jpg"
+            },
+                new Proyecto
+            {
+                Titulo = "AG Technology",
+                Descripcion = "E-Commerce realizada en Wordpress",
+                Link = "https://agtechnology.com.ar/",
+                ImagenURL = "/imagenes/agt.jpg"
+            },
+                new Proyecto
+            {
+                Titulo = "Benitegarcia",
+                Descripcion = "Sitio Web realizado en Wordpress",
+                Link = "http://benitegarcia.com/",
+                ImagenURL = "/imagenes/benite.jpg"
+            },
+                new Proyecto
+            {
+                Titulo = "ConstruMeza",
+                Descripcion = "Sitio Web realizado en Wordpress",
+                Link = "https://construmeza.com/",
+                ImagenURL = "/imagenes/construmeza.jpg"
+            },
+            };
+        }
+    }
+}
